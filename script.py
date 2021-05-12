1f5106f25375ca5732e932f1631b18beaac2da19
prkhrkat <39842461+prkhrkat@users.noreply.github.com>
2021-05-12 20:57:47 +0530

    Update script.py

--- script.py
+++ script.py
@@ -71,10 +71,11 @@ def ssh_and_send_command(instance_id):
         # Execute a command(cmd) after connecting/ssh to an instance
         stdin, stdout, stderr = client.exec_command(cmd)
         print(stdout.read())
-        a = stdout.read()
+        a = []
+        a.append(stdout.read())
         # close the client connection once the job is done
         client.close()
-        return a
+        return a[0]
 
     except:
         return 'getting some error'
